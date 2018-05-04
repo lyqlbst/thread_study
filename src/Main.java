2315a7c73a7145ac5e9dc419568d868c4feb8ef0
linyuqiang <linyuqiang@bonc.com.cn>
2018-05-04 12:01:17 +0800

    thread.interrupt()

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        byte[] bytes = new byte[1024 * 1000 * 30];
+//        byte[] bytes1 = new byte[1024 * 1000 * 20];
+        System.out.println("Hello World!");
+    }
+}
--- src/thread_stop/MyMain.java
+++ src/thread_stop/MyMain.java
@@ -3,8 +3,14 @@ package thread_stop;
 class MyMain {
 
     public static void main(String[] args) {
-        MyThreadCannotStop thread = new MyThreadCannotStop();
+        MyThreadCanStop thread = new MyThreadCanStop();
         thread.start();
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         thread.interrupt();
+        System.out.println("是否停止？="+thread.isInterrupted());
     }
 }
